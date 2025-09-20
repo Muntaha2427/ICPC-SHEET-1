697b7ddfed6c9c75867f08b05026ce3fff10bae0
Muntaha2427 <muntahamazumder612@gmail.com>
2025-09-21 01:21:41 +0600

    20.09.2025 HW

---
+++ O.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long A,B;
+    char S;
+    cin>>A>>S>>B;
+    if(S=='+')
+        cout<<A+B<<endl;
+    else if(S=='-')
+        cout<<A-B<<endl;
+    else if(S=='*')
+        cout<<A*B<<endl;
+    else if(S=='/')
+        cout<<A/B<<endl;
+    return 0;
+
+}
