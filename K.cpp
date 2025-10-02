386f74f7c023774b246347393243197a56682a6c
Muntaha2427 <muntahamazumder612@gmail.com>
2025-10-03 03:00:15 +0600

    Update K.cpp

---
+++ K.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long A,B,C;
+    cin>>A>>B>>C;
+    if(A<=B && A<=C )
+        cout<<A<<" ";
+    else if(B<=A && B<=C)
+        cout<<B<<" ";
+    else if(C<=A && C<=B)
+        cout<<C<<" ";
+    if(A>=B && A>=C )
+        cout<<A<<endl;
+    else if(B>=A && B>=C)
+        cout<<B<<endl;
+    else if(C>=A && C>=B)
+        cout<<C<<endl;
+    return 0;
+}
